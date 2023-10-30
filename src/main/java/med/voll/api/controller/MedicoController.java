e55c902f81fe85296bb2af33b149e65578527e49
Gabriel Resende <gabriel.resende@develcode.com.br>
2023-10-30 14:10:48 -0300

    findAll medicos com paginacao

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,12 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.model.medico.DadosCadastroMedico;
+import med.voll.api.model.medico.DadosListagemMedico;
 import med.voll.api.service.MedicoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medicos")
@@ -19,5 +21,10 @@ public class MedicoController {
     @PostMapping
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dadosCadastroMedico) {
         medicoService.cadastrar(dadosCadastroMedico);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemMedico>> listar(Pageable paginacao) {
+        return ResponseEntity.status(HttpStatus.OK).body(medicoService.listar(paginacao));
     }
 }
--- src/main/java/med/voll/api/service/MedicoService.java
+++ src/main/java/med/voll/api/service/MedicoService.java
@@ -1,11 +1,16 @@
 package med.voll.api.service;
 
 import med.voll.api.model.medico.DadosCadastroMedico;
+import med.voll.api.model.medico.DadosListagemMedico;
 import med.voll.api.model.medico.Medico;
 import med.voll.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class MedicoService {
@@ -16,5 +21,9 @@ public class MedicoService {
     @Transactional
     public void cadastrar(DadosCadastroMedico dadosCadastroMedico) {
         medicoRepository.save(new Medico(dadosCadastroMedico));
+    }
+
+    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+        return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
