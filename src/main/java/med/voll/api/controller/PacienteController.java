438271d6e167c4b67d9e7d5c4e5c1318eda18aa2
Gabriel Resende <gabriel.resende@develcode.com.br>
2023-10-30 14:45:27 -0300

    findAll pacientes ordenados e paginados

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,12 +2,15 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.model.paciente.DadosCadastroPaciente;
+import med.voll.api.model.paciente.DadosListagemPaciente;
 import med.voll.api.service.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/pacientes")
@@ -19,5 +22,10 @@ public class PacienteController {
     @PostMapping
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dadosCadastroPaciente) {
         pacienteService.cadastrar(dadosCadastroPaciente);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemPaciente>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginaco) {
+        return ResponseEntity.status(HttpStatus.OK).body(pacienteService.listar(paginaco));
     }
 }
--- src/main/java/med/voll/api/service/PacienteService.java
+++ src/main/java/med/voll/api/service/PacienteService.java
@@ -1,9 +1,12 @@
 package med.voll.api.service;
 
 import med.voll.api.model.paciente.DadosCadastroPaciente;
+import med.voll.api.model.paciente.DadosListagemPaciente;
 import med.voll.api.model.paciente.Paciente;
 import med.voll.api.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,7 +17,11 @@ public class PacienteService {
     private PacienteRepository pacienteRepository;
 
     @Transactional
-    public void cadastrar(DadosCadastroPaciente dadosCadastroPaciente){
+    public void cadastrar(DadosCadastroPaciente dadosCadastroPaciente) {
         pacienteRepository.save(new Paciente(dadosCadastroPaciente));
+    }
+
+    public Page<DadosListagemPaciente> listar(Pageable paginacao) {
+        return pacienteRepository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
